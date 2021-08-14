225e46242103e952eb054117edd643b43dddfcce
asman03 <assulaeman3@gmail.com>
2021-08-14 11:25:46 +0700

    add callback midtrans

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -16,6 +16,7 @@ use App\TransactionDetail;
 use Exception;
 use Midtrans\Config;
 use Midtrans\Snap;
+use Midtrans\Notification;
 
 class CheckoutController extends Controller
 {
@@ -52,7 +53,7 @@ class CheckoutController extends Controller
             ]);
         }
 
-        //hapus cart ssetelah transaksi
+        //hapus cart setelah transaksi
         Cart::where('users_id', Auth::user()->id)->delete();
 
 
@@ -88,10 +89,11 @@ class CheckoutController extends Controller
                 
                 // Redirect to Snap Payment Page
                 // header('Location: ' . $paymentUrl); -> jarang running
-
+                
                 return redirect($paymentUrl);
             }
             catch (Exception $e) {
+                // dd($serverKey);
             echo $e->getMessage();
             }
 
@@ -99,6 +101,53 @@ class CheckoutController extends Controller
 
     public function callback(Request $request)
     {
-        
+        //Set konfigurasi midtrans
+        Config::$serverKey = config('services.midtrans.serverKey');
+        Config::$isProduction = config('services.midtrans.isProduction');
+        Config::$isSanitized = config('services.midtrans.isSanitized');
+        Config::$is3ds = config('services.midtrans.is3ds');
+
+        //Instance midtrans notification
+        $notification = new Notification();
+
+        //Assign ke variable untuk memudahkan coding
+        $status = $notification->transaction_status;
+        $type = $notification->payment_type;
+        $fraud = $notification->fraud_status;
+        $order_id = $notification->order_id;
+
+        //Cari transaksi berdasarkan ID
+        $transaction = Transaction::findOrFail($order_id);
+
+        //Handle notification status
+        if($status == 'capture') {
+            if($type == 'credit_card') {
+                if($fraud == 'challenge') {
+                    $transaction->status ='PENDING';
+                }
+                else {
+                    $transaction->status = 'SUCCESS';
+                }
+            }
+        }
+
+        else if($status == 'settlement') {
+            $transaction->status = 'SUCCESS';
+        }
+        else if($status == 'pending') {
+            $transaction->status = 'PENDING';
+        }
+        else if($status == 'deny') {
+            $transaction->status = 'CANCELED';
+        }
+        else if($status == 'expire') {
+            $transaction->status = 'CANCELED';
+        }
+        else if($status == 'cancel') {
+            $transaction->status = 'CANCELED';
+        }
+
+        //simpan transaksi
+        $transaction->save();
     }
 }
