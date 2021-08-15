4400dcab9d38a10fb7157f8a0e6e929defe455d5
asman03 <assulaeman3@gmail.com>
2021-08-15 21:27:50 +0700

    move route to api

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/register/check', 'Auth\RegisterController@check')->name('api-register-check');
+Route::post('/checkout/callback', 'CheckoutController@callback')->name('midtrans-checkout');
+
 
